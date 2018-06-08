518bd8ff151cc028ee4206ae41a7e9d2fdf8732f
cc <curt.caldwell24@gmail.com>
2018-06-08 15:07:54 -0700

    tryto add inset name box, failed terribly.

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,25 @@
 function boop(input) {
 
-  var results = [];
-  for (var i = 0; i <= input; i++) {
+  var output = [];
+  var nan = "Please Use Numbers Only"
 
+    if (isNaN(input)) {
+      alert(nan);
+    }
+  for (var i = 1; i <= input; i++) {
     if (i % 3 == 0 ) {
-      results.push("I'm sorry, Dave. I'm afraid I can't do that.");
+      output.push("I'm sorry Dave, I'm afraid I can't do that.");
     } else if (i.toString().match(/0/)) {
-      results.push("Beep!")
+      output.push("Beep!")
 
     } else if (i.toString().match(/1/)) {
-      results.push("Boop!")
+      output.push("Boop!")
 
     } else {
-      results.push(i);
+      output.push(i);
     }
   }
-  alert(results);
+    alert(output);
 }
 
 
@@ -28,6 +32,7 @@ $(function(){
   $("form#number").submit(function(event){
     event.preventDefault();
     var input = parseInt($("input#number").val());
+    var name = parseInt($("input#name").val());
     boop(input);
 
   });
